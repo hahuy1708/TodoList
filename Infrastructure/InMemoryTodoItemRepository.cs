c18fb697557a4799e05fa2be832673a243c0f29c
hahuy1708 <hahuy17082005@gmail.com>
2025-05-15 07:55:10 +0700

    add InMemory's implementation

---
+++ Infrastructure/InMemoryTodoItemRepository.cs
@@ -0,0 +1,45 @@
+﻿using Entities;
+using UseCases;
+
+namespace Infrastructure;
+
+public class InMemoryTodoItemRepository : ITodoListRepository
+{
+    private List<TodoItem> _items;
+
+    public InMemoryTodoItemRepository()
+    {
+        _items = [];
+    }
+
+    public void AddTodoItems(TodoItem item)
+    {
+        _items.Add(item);
+    }
+
+    public void Delete(int id)
+    {
+        var item = _items.FirstOrDefault(i => i.ID == id);
+        if(item != null) _items.Remove(item);
+    }
+
+    public TodoItem GetById(int id)
+    {
+        return _items.FirstOrDefault(i => i.ID == id);
+    }
+
+    public IEnumerable<TodoItem> GetTodoItems()
+    {
+        return _items;
+    }
+
+    public void Update(TodoItem item)
+    {
+        var existingItem = _items.FirstOrDefault(i => i.ID == item.ID);
+        if (existingItem != null)
+        {
+            existingItem.Text = item.Text;
+            existingItem.IsCompleted = item.IsCompleted;
+        }
+    }
+}
--- UseCases/ITodoListRepository.cs
+++ UseCases/ITodoListRepository.cs
@@ -7,7 +7,7 @@ public interface ITodoListRepository
     IEnumerable<TodoItem> GetTodoItems();
     void AddTodoItems(TodoItem item);
     TodoItem GetById(int id);
-    void Update(TodoItem? item);
+    void Update(TodoItem item);
     void Delete(int id);
     
 }
